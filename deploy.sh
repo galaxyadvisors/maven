de1c433bb7c672f96ed9b0bd7c8e6b9cd6f908a1
Marton Makai <marton.makai@gmail.com>
2015-04-01 15:11:13 +0200

    Deploy EWS API snapshot version 2.0-20150331.184334-7.

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@
 set -eux
 
 mvn deploy:deploy-file -Dversion=2.0-20150321.214216-2 -DpomFile=ews-java-api-2.0-20150321.214216-2.pom -Dfile=ews-java-api-2.0-20150321.214216-2.jar -Durl=file:snapshots
+mvn deploy:deploy-file -Dversion=2.0-20150331.184334-7 -DpomFile=ews-java-api-2.0-20150331.184334-7.pom -Dfile=ews-java-api-2.0-20150331.184334-7.jar -Durl=file:snapshots
