2f78f53e8395db85d45fdc3f726214e000a30c0a
Hauke Fuehres <hfuehres@galaxyadvisors.com>
2015-05-05 08:21:11 +0200

    deployment of version 2.0-20150430.152849-22

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -eux
 
 # Command to push a jar and pom to the repository. Update the command with
 # your jar and pom and version.
-mvn deploy:deploy-file -Dversion=2.0-20150407.093737-16 -DpomFile=ews-java-api-2.0-20150407.093737-16.pom -Dfile=ews-java-api-2.0-20150407.093737-16.jar -Durl=file:snapshots
+mvn deploy:deploy-file -Dversion=2.0-20150430.152849-22 -DpomFile=ews-java-api-2.0-20150430.152849-22.pom -Dfile=ews-java-api-2.0-20150430.152849-22.jar -Durl=file:snapshots
