d86b2fc1d04dff8d41554caedb909b66dc135fca
Marton Makai <marton.makai@gmail.com>
2015-05-28 18:53:00 +0200

    Deploy EWS API snapshot version 2.0-20150516.173435-26.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ set -eux
 
 # Command to push a jar and pom to the repository. Update the command with
 # your jar and pom and version.
-mvn deploy:deploy-file -Dversion=2.0-20150430.152849-22 -DpomFile=ews-java-api-2.0-20150430.152849-22.pom -Dfile=ews-java-api-2.0-20150430.152849-22.jar -Durl=file:snapshots
+version="2.0-20150516.173435-26"
+mvn deploy:deploy-file -Dversion=${version} -DpomFile=ews-java-api-${version}.pom -Dfile=ews-java-api-${version}.jar -Durl=file:snapshots
